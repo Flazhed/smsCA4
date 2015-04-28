735ca60ac2ae03bd38fd9cce2059eaa9798006c8
Schultzz <ms.fuge@gmail.com>
2015-04-28 11:48:03 +0200

    REST-API done, view1 created

--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -1,47 +1,79 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var user = mongoose.model('User');
+//var mongoose = require('mongoose');
+//var user = mongoose.model('User');
 
+var wiki = require('../model/wikiFacade');
+
+// Some random aids!
 /* GET A User From The DataBase */
-router.get('/user', function (req, res) {
-    if (typeof global.mongo_error !== "undefined") {
-        res.status(500);
-        res.end("Error: " + global.mongo_error + " To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
-        return;
-    }
-    user.find({}, function (err, users) {
-        if (err) {
-            res.status(err.status || 400);
-            res.end(JSON.stringify({error: err.toString()}));
-            return;
-        }
-        res.header("Content-type", "application/json");
-        res.end(JSON.stringify(users));
-    });
-});
+//router.get('/user', function (req, res) {
+//    if (typeof global.mongo_error !== "undefined") {
+//        res.status(500);
+//        res.end("Error: " + global.mongo_error + " To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+//        return;
+//    }
+//    user.find({}, function (err, users) {
+//        if (err) {
+//            res.status(err.status || 400);
+//            res.end(JSON.stringify({error: err.toString()}));
+//            return;
+//        }
+//        res.header("Content-type", "application/json");
+//        res.end(JSON.stringify(users));
+//    });
+//});
 
 router.get('/wiki/getWiki/:title', function (req, res) {
     var title = req.params.title;
 
-
+    wiki.getWiki(title, function (err, elem) {
+        if (err) {
+            return res.end(JSON.stringify({error: err.toString()}))
+        }
+        ;
+        res.end(elem);
+    })
 
 });
 
 router.get('/wiki/findWiki/:title', function (req, res) {
     var title = req.params.title;
 
+    wiki.findWiki(title, function (err, elem) {
+        if (err) {
+            return res.end(JSON.stringify({error: err.toString()}))
+        }
+
+        res.end(elem);
+
+    })
 
 });
 
 router.get('/wiki/getCategories', function (req, res) {
 
+    wiki.getCategories(function (err, elem) {
+        if (err) {
+            return res.end(JSON.stringify({error: err.toString()}))
+        }
+        ;
+        res.end(elem);
+    })
+
 });
 
-router.get('/wiki/getWikisWithCategory/:category', function (req, res) {
-    var category = req.params.category;
-
+router.get('/wiki/getCategories/:cat', function (req, res) {
+    var category = req.params.cat;
+    console.log(category);
+    wiki.getWikiWithCategory(category, function (err, elem) {
+        if (err) {
+            return res.end(JSON.stringify({error: err.toString()}))
+        }
+        ;
+        res.end(elem);
+    });
 
 });
 
