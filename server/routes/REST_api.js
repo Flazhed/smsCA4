c1335936b49c3552a7eee5fcdcb37d9b572e55f7
Schultzz <ms.fuge@gmail.com>
2015-04-28 09:33:24 +0200

    REST-API created, mongooseScheme updated

--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -5,21 +5,45 @@ var mongoose = require('mongoose');
 var user = mongoose.model('User');
 
 /* GET A User From The DataBase */
-router.get('/user', function(req, res) {
-  if(typeof global.mongo_error !== "undefined"){
-    res.status(500);
-    res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
-    return;
-  }
-  user.find({}, function (err, users) {
-    if (err) {
-      res.status(err.status || 400);
-      res.end(JSON.stringify({error: err.toString()}));
-      return;
+router.get('/user', function (req, res) {
+    if (typeof global.mongo_error !== "undefined") {
+        res.status(500);
+        res.end("Error: " + global.mongo_error + " To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+        return;
     }
-    res.header("Content-type","application/json");
-    res.end(JSON.stringify(users));
-  });
+    user.find({}, function (err, users) {
+        if (err) {
+            res.status(err.status || 400);
+            res.end(JSON.stringify({error: err.toString()}));
+            return;
+        }
+        res.header("Content-type", "application/json");
+        res.end(JSON.stringify(users));
+    });
 });
+
+router.get('/wiki/getWiki/:title', function (req, res) {
+    var title = req.params.title;
+
+
+
+});
+
+router.get('/wiki/findWiki/:title', function (req, res) {
+    var title = req.params.title;
+
+
+});
+
+router.get('/wiki/getCategories', function (req, res) {
+
+});
+
+router.get('/wiki/getWikisWithCategory/:category', function (req, res) {
+    var category = req.params.category;
+
+
+});
+
 
 module.exports = router;
